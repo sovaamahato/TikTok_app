2ad0791edcf97d526e135c2b0ed9ed4fc1698fda
sova <mahatoshobha143@gmail.com>
2023-05-10 22:42:22 +0545

    firebase setup done for android

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
-import 'package:get/get_navigation/src/root/get_material_app.dart';
+//import 'package:get/get_core/src/get_main.dart';
+//import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:titctok_clone/pages/authentication_controller.dart';
 
 import 'pages/login_screen.dart';
--- lib/pages/signup_screen.dart
+++ lib/pages/signup_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
+// import 'package:get/get_core/src/get_main.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:titctok_clone/pages/authentication_controller.dart';
 
