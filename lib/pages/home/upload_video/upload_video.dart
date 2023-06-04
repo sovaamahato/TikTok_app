22852241d6564e58aaf6a63a5967cf9eab1b83c1
sova <mahatoshobha143@gmail.com>
2023-06-04 22:44:22 +0545

    dialog box added for uploading video

--- lib/pages/home/upload_video/upload_video.dart
+++ lib/pages/home/upload_video/upload_video.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class UploadVideo extends StatefulWidget {
   const UploadVideo({super.key});
@@ -8,27 +9,102 @@ class UploadVideo extends StatefulWidget {
 }
 
 class _UploadVideoState extends State<UploadVideo> {
+  Future ShowDialogBox() {
+    return showDialog(
+        context: context,
+        builder: ((context) => SimpleDialog(
+              children: [
+                SimpleDialogOption(
+                  onPressed: () {},
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.image,
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding: EdgeInsets.all(8),
+                          child: Text(
+                            "Get video from Gallery",
+                            maxLines: 3,
+                            style: TextStyle(fontSize: 14),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+
+
+                SimpleDialogOption(
+                  onPressed: () {},
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.camera_alt,
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding: EdgeInsets.all(8),
+                          child: Text(
+                            "Capture video with camera",
+                            maxLines: 3,
+                            style: TextStyle(fontSize: 14),
+                          ),
+                        ),
+                      ),
+
+
+                    ],
+                  ),
+                ),
+
+
+                SimpleDialogOption(
+                  onPressed: () {
+
+                    Get.back();
+                  },
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.cancel,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(8),
+                        child: Text(
+                          "Cancel",
+                          style: TextStyle(fontSize: 14),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            )));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          
           children: [
             Image.asset(
               "images/upload_video1.jpg",
               width: 200,
             ),
             ElevatedButton(
-              
                 style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
-                onPressed: () {},
+                onPressed: () {
+                  ShowDialogBox();
+                },
                 child: Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: Text(
                     "Upload Video",
-                    style: TextStyle(fontSize: 17,fontWeight: FontWeight.bold),
+                    style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
                   ),
                 ))
           ],
